1d2e2d87dff84cc2d765d6fc34531aafc60cfc08
DiegoPre <arturopre@hotmail.com>
2023-09-06 10:38:08 -0500

    version 0.1

---
+++ src/app/Components/crea-tu-cuenta/crea-tu-cuenta.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crea-tu-cuenta',
+  templateUrl: './crea-tu-cuenta.component.html',
+  styleUrls: ['./crea-tu-cuenta.component.css']
+})
+export class CreaTuCuentaComponent {
+  titulo="SEA PARTE DE NUESTROS CLIENTES SATISFECHOS"
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './Components/menu/menu.component';
+import { ProductosComponent } from './Components/productos/productos.component';
+import { ContactenosComponent } from './Components/contactenos/contactenos.component';
+import { NosotrosComponent } from './Components/nosotros/nosotros.component';
+import { NuestrosClientesComponent } from './Components/nuestros-clientes/nuestros-clientes.component';
+import { CreaTuCuentaComponent } from './Components/crea-tu-cuenta/crea-tu-cuenta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ProductosComponent,
+    ContactenosComponent,
+    NosotrosComponent,
+    NuestrosClientesComponent,
+    CreaTuCuentaComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
